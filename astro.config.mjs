874b6db3002e6ce4cac34db8bb8395b5f8803a7c
sachin-veersoft <sachin@veersoftsolutions.com>
2026-04-22 08:19:16 +0530

    add bookworm blog theme to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,57 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import AutoImport from "astro-auto-import";
+import { defineConfig, sharpImageService } from "astro/config";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import remarkCollapse from "remark-collapse";
+import remarkToc from "remark-toc";
+import config from "./src/config/config.json";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "server",
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
     },
   }),
+  site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
+  base: config.site.base_path ? config.site.base_path : "/",
+  trailingSlash: config.site.trailing_slash ? "always" : "never",
+  image: { service: sharpImageService() },
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": `${__dirname}/src`,
+      },
+    },
+  },
+  integrations: [
+    react(),
+    sitemap(),
+    AutoImport({
+      imports: [
+        "@/shortcodes/Button",
+        "@/shortcodes/Accordion",
+        "@/shortcodes/Notice",
+        "@/shortcodes/Video",
+        "@/shortcodes/Youtube",
+        "@/shortcodes/Tabs",
+        "@/shortcodes/Tab",
+      ],
+    }),
+    mdx(),
+  ],
+  markdown: {
+    remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
+    shikiConfig: { theme: "one-dark-pro", wrap: true },
   },
 });
