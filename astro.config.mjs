663786f006f8f857c62c821c624161c0f24a25ad
sachin-veersoft <sachin@veersoftsolutions.com>
2026-07-20 17:58:51 +0530

    Update .gitignore, modify config settings, and enhance blog and search page headings

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,7 +24,6 @@ export default defineConfig({
   }),
   site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
   base: config.site.base_path ? config.site.base_path : "/",
-  trailingSlash: config.site.trailing_slash ? "always" : "never",
   image: { service: sharpImageService() },
   vite: {
     plugins: [
